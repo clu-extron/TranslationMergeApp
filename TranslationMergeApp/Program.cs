06a3ce5b94a7372bf2948881914596f4b7b52ea8
Chaoran Lu <clu@extron.com>
2023-06-21 15:37:19 -0700

    Add project files.

---
+++ TranslationMergeApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslationMergeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merge Start");
+
+            string sourceFile = "C:\\Learn\\Files\\Test\\TLSDeskResources.json";
+            string destinationFile = "C:\\Learn\\Files\\Test\\TLSDeskResources{zh-Hant}.json";
+
+            JsonComparer.MergeJsonFiles(sourceFile, destinationFile);
+
+            Console.WriteLine("Merge Fini");
+        }
+    }
+}
---
+++ TranslationMergeApp/ResxComparer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.Design;
+using System.Resources;
+
+public class ResxComparer
+{
+    public static void MergeResxFiles(string sourceFilePath, string destinationFilePath)
+    {
+        // Write the merged resources back to the destination file
+        using (var destinationWriter = new ResXResourceWriter(destinationFilePath))
+        {
+            // Read the source .resx file
+            using (var sourceReader = new ResXResourceReader(sourceFilePath))
+            {
+                sourceReader.UseResXDataNodes = true;
+                // Read the destination .resx file
+                using (var destinationReader = new ResXResourceReader(destinationFilePath))
+                {
+                    IDictionaryEnumerator srcEnumerator = sourceReader.GetEnumerator();
+                    while (srcEnumerator.MoveNext())
+                    {
+                        string key = srcEnumerator.Key.ToString();
+                        object value = srcEnumerator.Value;
+
+                        IDictionaryEnumerator destEnumerator = destinationReader.GetEnumerator();
+                        while (destEnumerator.MoveNext())
+                        {
+                            string destKey = destEnumerator.Key.ToString();
+                            if (string.Equals(destKey, key, StringComparison.OrdinalIgnoreCase))
+                            {
+                                string comment = "";
+                                ResXDataNode dataNode = (ResXDataNode)srcEnumerator.Value;
+                                if (dataNode != null && dataNode.Comment != null)
+                                {
+                                    comment = dataNode.Comment;
+                                }
+                                if (string.IsNullOrEmpty((string)destEnumerator.Value))
+                                {
+                                    if (!string.IsNullOrEmpty(comment))
+                                    {
+                                        ResXDataNode resultNode = new ResXDataNode(key, dataNode.GetValue((ITypeResolutionService)null))
+                                        {
+                                            Comment = comment
+                                        };
+                                        destinationWriter.AddResource(resultNode);
+                                    } else
+                                    {
+                                        destinationWriter.AddResource(key, value);
+                                    }
+                                    
+                                }
+                                else
+                                {
+                                    if (!string.IsNullOrEmpty(comment))
+                                    {
+                                        ResXDataNode resultNode = new ResXDataNode(key, destEnumerator.Value)
+                                        {
+                                            Comment = comment
+                                        };
+                                        destinationWriter.AddResource(resultNode);
+                                    }
+                                    else
+                                    {
+                                        destinationWriter.AddResource(key, destEnumerator.Value);
+                                    }  
+                                }
+
+                            }
+                        }
+                    }
+                }
+
+                // Save the changes to the destination file
+                destinationWriter.Generate();
+            }
+
+            Console.WriteLine("Resx files merged successfully!");
+        }
+    }
+}
