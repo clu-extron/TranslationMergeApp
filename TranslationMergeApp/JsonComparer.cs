107b29b39dc84f049c5fa781dd043900cd31fd61
Chaoran Lu <clu@extron.com>
2023-06-21 16:11:03 -0700

    Add support for Json merge

--- TranslationMergeApp/JsonComparer.cs
+++ TranslationMergeApp/JsonComparer.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,28 @@ namespace TranslationMergeApp
 {
     public class JsonComparer
     {
+        public static void MergeJsonFiles(string sourcePath, string destinationPath)
+        {
+            // Read the contents of both JSON files
+            string srcJson = File.ReadAllText(sourcePath);
+            string destJson = File.ReadAllText(destinationPath);
+
+            // Parse the JSON data into JObjects
+            JObject srcData = JObject.Parse(srcJson);
+            JObject destData = JObject.Parse(destJson);
+
+            // Iterate over each property in the source JSON file
+            foreach (var property in srcData.Properties())
+            {
+                // Check if the property is empty in the src JSON file
+                if (string.IsNullOrEmpty((string)destData[property.Name]["string"]))
+                {
+                    destData[property.Name]["string"] = property.Value["string"];
+                }
+            }
+
+            //Write the merged JSON data to the destination file
+            File.WriteAllText(destinationPath, destData.ToString());
+        }
     }
 }
